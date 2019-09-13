c353e32f048ee95ee1dfaffe51f653994a24bd22
Nicholas Truson <nicholas.truson@gmail.com>
2019-09-13 09:06:33 -0700

    initial actions component setup

---
+++ smurfs/src/actions/smurfActions.js
@@ -0,0 +1,47 @@
+// AXIOS
+
+import axios from 'axios';
+
+// ACTION EXPORTS
+
+export const FETCH_SMURF_DATA_START = 'FETCH_SMURF_DATA_START';
+export const FETCH_SMURF_DATA_SUCCESS = 'FETCH_SMURF_DATA_SUCCESS';
+export const FETCH_SMURF_DATA_FAILURE = 'FETCH_SMURF_DATA_FAILURE';
+export const SEND_SMURF_DATA_SUCCESS = 'SEND_SMURF_DATA_SUCCESS';
+
+// AXIOS.POST DATA 
+
+export const sendData = () => {
+    return dispatch => {
+        axios
+        // POST
+        .post('http://localhost:3333/smurfs', {
+            name: '',
+            age: [],
+            height: '',
+            id: []
+        })
+        .then(res => {
+            dispatch({ type: SEND_SMURF_DATA_SUCCESS, payload: res.data});
+        })
+    }
+}
+
+// AXIOS.GET DATA
+
+export const getData = () => {
+  return dispatch => {
+    dispatch({ type: FETCH_SMURF_DATA_START });
+    axios
+    //GET
+      .get('http://localhost:3333/smurfs')
+      .then(res => {
+        console.log(res.data);
+        dispatch({ type: FETCH_SMURF_DATA_SUCCESS, payload: res.data});
+      })
+    // CATCH
+      .catch(err => {
+        dispatch({ type: FETCH_SMURF_DATA_FAILURE, payload: err.response });
+      });
+  };
+};
